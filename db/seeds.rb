79320d09410b985f39a25e6b3b64018c8c04547b
Heather Doucet <heatherredoucet@gmail.com>
2017-02-21 12:36:12 -0600

    More Seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Tour.create!(date_time: '09/02/2017', venue: 'Altamont Theatre', location: 'Ashville, NC', imgUrl: 'www.thealtamonttheatre.com', img: 'assets/portfolio/altamont-logo-lrg-1.jpg')
+Tour.create!(date_time: 'Thursday, February 9, TBD', venue: 'Altamont Theatre', location: 'Ashville, NC', imgUrl: 'www.thealtamonttheatre.com', img: 'assets/portfolio/altamont-logo-lrg-1.jpg')
+Tour.create!(date_time: 'Friday, February 10, 7:30pm', venue: 'Hiwassee Performing Arts Center', location: 'Hopkinsville, KY', imgUrl: 'www.hiwassee.edu/events', img: 'assets/portfolio/Hiwasseelogo.png')
+
+puts "Created #{Tour.count} tours"
