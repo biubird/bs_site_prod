e021dbd1ea69ca55bf11550a833854c5eff144da
Heather Doucet <heatherredoucet@gmail.com>
2017-02-21 16:55:00 -0600

    adding tour dates to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,54 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Tour.create!(date_time: 'Thursday, February 9, TBD', venue: 'Altamont Theatre', location: 'Ashville, NC', imgUrl: 'www.thealtamonttheatre.com', img: 'assets/portfolio/altamont-logo-lrg-1.jpg')
-Tour.create!(date_time: 'Friday, February 10, 7:30pm', venue: 'Hiwassee Performing Arts Center', location: 'Hopkinsville, KY', imgUrl: 'www.hiwassee.edu/events', img: 'assets/portfolio/Hiwasseelogo.png')
+Tour.create!(date_time: 'Thursday, February 9, TBD', venue: 'Altamont Theatre', location: 'Asheville, NC', imgUrl: 'http://thealtamonttheatre.com', img: 'assets/portfolio/altamont-logo-lrg-1.jpg')
+
+Tour.create!(date_time: 'Friday, February 10, 7:30pm', venue: 'Hiwassee Performing Arts Center', location: 'Madisonville, TN', imgUrl: 'https://www.hiwassee.edu/events', img: 'assets/portfolio/Hiwasseelogo.png')
+
+Tour.create!(date_time: 'Saturday, February 11, 7:30pm', venue: 'Alhambra Theatre', location: 'Hopkinsville, KY', imgUrl: 'http://www.pennyroyalarts.org', img: 'assets/portfolio/alhambratheatre.jpg')
+
+Tour.create!(date_time: 'Sunday, February 12, 7:00pm', venue: 'Buckman Performing Arts Center', location: 'Memphis, TN', imgUrl: 'http://www.stmarysschool.org/page.cfm?p=668', img: 'assets/portfolio/buckmanpac.png')
+
+Tour.create!(date_time: 'Friday, February 17, 8:00pm', venue: 'Center for the Performing Arts', location: 'Bonita Springs, FL', imgUrl: 'http://artcenterbonita.org', img: 'assets/portfolio/bonitasprings.jpg')
+
+Tour.create!(date_time: 'Saturday, February 18, 8:00pm', venue: 'Palladium Theater', location: 'St. Petersburg, FL', imgUrl: 'http://www.mypalladium.org', img: 'assets/portfolio/palladium.jpg')
+
+Tour.create!(date_time: 'Sunday, February 19, 8:30pm', venue: 'Original Cafe Eleven', location: 'St. Augustine, FL', imgUrl: 'http://www.originalcafe11.com', img: 'assets/portfolio/cafe11.jpg')
+
+Tour.create!(date_time: 'Thursday, March 2, 6:30pm', venue: 'Laurie  Auditorium', location: 'San Antonio, TX', imgUrl: 'https://new.trinity.edu/about-trinity/campus/laurie-auditorium', img: 'assets/portfolio/laurie.jpg')
+
+Tour.create!(date_time: 'Friday, March 3, 7:30pm', venue: 'Laurie  Auditorium', location: 'San Antonio, TX', imgUrl: 'https://new.trinity.edu/about-trinity/campus/laurie-auditorium', img: 'assets/portfolio/laurie.jpg')
+
+Tour.create!(date_time: 'Saturday, March 4, 7:30pm', venue: 'Reif  Performing Arts Center', location: 'Grand Rapids, MN', imgUrl: 'http://www.reifcenter.org', img: 'assets/portfolio/reif.jpg')
+
+Tour.create!(date_time: 'Saturday, April 15, 7:30pm', venue: 'Taylor-Meade Performing Arts Center', location: 'Forest Grove, OR', imgUrl: 'https://www.pacificu.edu/about-us/events/performing-arts-series', img: 'assets/portfolio/pacificu.jpg')
+
+Tour.create!(date_time: 'Monday, April 17, 8:30pm', venue: 'Mystic Theatre', location: 'Petaluma, CA', imgUrl: 'http://www.ticketweb.com/t3/sale/SaleEventDetail?dispatch=loadSelectionData&eventId=7175985&pl=mystic', img: 'assets/portfolio/mystic.jpg')
+
+Tour.create!(date_time: 'Tuesday, April 18, 6:30pm', venue: 'Christopher Cohan Performing Arts Center', location: 'San Luis Obispo, CA', imgUrl: 'https://calpolyarts.org/Online/default.asp?doWork::WScontent::loadArticle=Load&BOparam::WScontent::loadArticle::article_id=8B1E82DC-8C14-4BCB-8C22-799ABE4A68A9', img: 'assets/portfolio/calpolyarts.jpg')
+
+Tour.create!(date_time: 'Wednesday, April 19, 7:30pm', venue: 'Spanos Theater', location: 'San Luis Obispo, CA', imgUrl: 'https://calpolyarts.org/Online/default.asp?doWork::WScontent::loadArticle=Load&BOparam::WScontent::loadArticle::article_id=8B1E82DC-8C14-4BCB-8C22-799ABE4A68A9', img: 'assets/portfolio/calpolyarts.jpg')
+
+Tour.create!(date_time: 'Friday, April 21, 7:00pm', venue: 'Museum of Making Music', location: 'Carlsbad, CA', imgUrl: 'https://www.museumofmakingmusic.org/events/beausoleil', img: 'assets/portfolio/museummusic.jpg')
+
+Tour.create!(date_time: 'Saturday, April 22, 8:00pm', venue: "McCabe's Guitar Shop", location: 'Santa Monica, CA', imgUrl: 'https://www.mccabes.com/concerts/', img: 'assets/portfolio/mccabes.jpg')
+
+Tour.create!(date_time: 'Saturday, May 6, TBD', venue: 'New Orleans Jazz & Heritage Festival', location: 'New Orleans, LA', imgUrl: 'http://www.nojazzfest.com/', img: 'assets/portfolio/nojazz.jpg')
+
+Tour.create!(date_time: 'Sunday, May 14, 3:00pm', venue: 'Paradise Bar and Grill', location: 'Pensacola Beach, FL', imgUrl: 'http://www.paradisebar-grill.com/', img: 'assets/portfolio/paradise.jpg')
+
+Tour.create!(date_time: 'Tuesday, May 16, 6:00pm', venue: 'Paradise Bar and Grill', location: 'Pensacola Beach, FL', imgUrl: 'http://www.paradisebar-grill.com/', img: 'assets/portfolio/paradise.jpg')
+
+Tour.create!(date_time: 'Thursday, May 25, TBD', venue: 'Mudbug Madness', location: 'Shreveport, LA', imgUrl: 'http://mudbugmadness.com/', img: 'assets/portfolio/mudbug.jpg')
+
+Tour.create!(date_time: 'Saturday, May 27 - Sunday, May 28, TBD', venue: 'Cajun & Blues Music Festival', location: 'Simi Valley, CA', imgUrl: 'http://www.simicajun.org/schedule/', img: 'assets/portfolio/simi.jpg')
+
+Tour.create!(date_time: 'June 28 - July 4, TBD', venue: 'Smithsonian Folklife Festival', location: 'Washington, DC', imgUrl: 'http://www.festival.si.edu/2017/nea-national-heritage-fellows/smithsonian', img: 'assets/portfolio/smithsonian.jpg')
+
+Tour.create!(date_time: 'Friday, August 25, 8:00pm', venue: 'Shalin Liu Performance Center', location: 'Rockport, MA', imgUrl: 'http://rockportmusic.org/shalin-liu-performance-center/', img: 'assets/portfolio/shalin.jpg')
+
+Tour.create!(date_time: 'Saturday, August 26, TBD', venue: 'Colonial Theatre', location: 'Bethlehem, MA', imgUrl: 'http://bethlehemcolonialtheatre.org/', img: 'assets/portfolio/colonial.jpg')
+
+Tour.create!(date_time: 'Thursday, October 5, 7:30pm', venue: 'Fox Theater', location: 'Spokane, WA', imgUrl: 'http://www.foxtheaterspokane.com/', img: 'assets/portfolio/foxtheater.jpg')
 
 puts "Created #{Tour.count} tours"
